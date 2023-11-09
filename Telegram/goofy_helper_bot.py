ed38e7c103264a3d71bf656c5471b9156c1a11c1
= <=>
2023-11-09 20:14:11 +0400

    Changes

--- Telegram/goofy_helper_bot.py
+++ Telegram/goofy_helper_bot.py
@@ -21,7 +21,7 @@ def start(message):
 
 @bot.message_handler(commands=['check'])
 def check(message):
-    bot.send_message(message.chat.id, 'Я включен и работаю как надо.')
+    bot.send_message(message.chat.id, 'Я включен.')
 
 
 @bot.callback_query_handler(func=lambda callback: True)
