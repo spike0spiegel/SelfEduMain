f3043d715ba3bfed017e346ba63d849f9731f3bb
= <=>
2023-11-10 13:24:14 +0400

    CI/CD check

--- Telegram/goofy_helper_bot.py
+++ Telegram/goofy_helper_bot.py
@@ -21,7 +21,7 @@ def start(message):
 
 @bot.message_handler(commands=['check'])
 def check(message):
-    bot.send_message(message.chat.id, 'Я включен.')
+    bot.send_message(message.chat.id, 'Я включен. Я умею в CI/CD.')
 
 
 @bot.callback_query_handler(func=lambda callback: True)
